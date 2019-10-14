bd7434560dde13ef11214cc7758687655d93b2df
pth1080 <halleyphuong0108@gmail.com>
2019-10-14 12:54:26 +0700

    add module

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,5 +1,5 @@
+import User from './user.router'
 const router = require('express').Router()
-
 // Create use routers
-
+router.use('/user', User)
 export default router
--- src/routers/v1/user.router.js
+++ src/routers/v1/user.router.js
@@ -8,3 +8,5 @@ let controller = new UserController()
 router.post('/login', authentication, controller.login)
 router.post('/register', controller.register)
 router.post('/update/password', controller.updatePassword)
+
+export default router
